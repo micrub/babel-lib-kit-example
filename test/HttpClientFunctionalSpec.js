3d9bd5479aa259b14549526d31059fc13722147e
Michael Rubanov <mrubanov@gmail.com>
2017-07-25 01:33:47 +0300

    @clean

--- test/HttpClientFunctionalSpec.js
+++ test/HttpClientFunctionalSpec.js
@@ -6,6 +6,7 @@ import Core from '../src/index';
 describe('Core.HttpClient module exports functional tests.', () => {
 
   const HttpClient = Core.HttpClient;
+
   it('should be instance of `Object` and has `Object` constructor.', () => {
     expect(HttpClient).to.be.instanceOf(Object);
     expect(HttpClient.constructor.name).to.be.eq('Object');
@@ -15,41 +16,44 @@ describe('Core.HttpClient module exports functional tests.', () => {
     expect(HttpClient.get).to.be.instanceOf(Function);
   })
 
-  describe('`get` function throws if first argument is empty string, or not a string',()=>{
-    it('`get` function throws if first argument is empty string, or not a string',()=>{
+  describe('`get` function input handling.',()=>{
+    it('should return EmptyUrlError if first argument is empty string, or not a string',()=>{
         let get = HttpClient.get('')
         expect(get).to.be.instanceOf(HttpClient.errors.EmptyUrlError);
     })
-    it('`get` function throws if first argument is invalid url.',()=>{
+    it('should return InvalidUrlError if first argument is invalid url.',()=>{
         let get = HttpClient.get('httx://google.com')
         expect(get).to.be.instanceOf(HttpClient.errors.InvalidUrlError);
     })
-    it('`get` function should return Promise in case of valid url.',()=>{
+    it('should return Promise in case of valid url.',()=>{
         let get = HttpClient.get('http://google.com')
         let cname = get.constructor.name;
         expect(get).to.be.instanceOf(Promise)
     })
-    it('`get` handle execution success in Promise convention. Return non empty string.',(done)=>{
+  })
+
+  describe('`get` Promisified execution handling tests.', ()=> {
+    it('execution success . Return non empty string.',(done)=>{
         let get = HttpClient.get('http://google.com')
         get.then((response) => {
           expect(typeof response).to.be.eq('string')
           done();
         })
     })
-    it('`get` handle execution failure in Promise convention. Return error object.',(done)=>{
-        let get = HttpClient.get('http://google.com/aasdasd')
-        get.catch((err) => {
-          expect(err).to.be.instanceOf(Object);
-          done();
-        })
-    })
-    it('`get` handle execution failure in Promise convention. Return 404 error status code.',(done)=>{
-        let get = HttpClient.get('http://google.com/aasdasd')
-        get.catch((err) => {
-          expect(err.statusCode).to.be.eq(404);
-          done();
-        })
-    })
+    //it('execution failure . Return error object.',(done)=>{
+        //let get = HttpClient.get('http://google.com/aasdasd')
+        //get.catch((err) => {
+          //expect(err).to.be.instanceOf(Object);
+          //done();
+        //})
+    //})
+    //it('execution failure . Return 404 error status code.',(done)=>{
+        //let get = HttpClient.get('http://google.com/aasdasd')
+        //get.catch((err) => {
+          //expect(err.statusCode).to.be.eq(404);
+          //done();
+        //})
+    //})
   })
 
 })

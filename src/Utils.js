c01db78ccc339214fef6118171d64e7e580c184f
Michael Rubanov <mrubanov@gmail.com>
2017-07-24 23:38:15 +0300

    bugfix

--- src/Utils.js
+++ src/Utils.js
@@ -3,7 +3,7 @@ import path from 'path';
 
 function jsBasename(filename) {
   if (typeof filename === 'string' && filename.length) {
-    return path.basename( __filename, '.js' ) || new Error('No basename detected for:' + filename);
+    return path.basename( filename, '.js' ) || new Error('No basename detected for:' + filename);
   } else {
     return new Error('Passed filename is not a string or empty: ' + filename);
   }
